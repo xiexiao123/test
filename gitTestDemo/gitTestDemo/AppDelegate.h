9fd901d889a4b7b32f8b7b67de9553a1f87cde5f
xiexiao123 <1337824083@qq.com>
2016-09-09 16:50:10 +0800

    初始化项目

---
+++ gitTestDemo/gitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTestDemo
+//
+//  Created by 王忠迪 on 9/9/16.
+//  Copyright © 2016 王忠迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTestDemo/gitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTestDemo
+//
+//  Created by 王忠迪 on 9/9/16.
+//  Copyright © 2016 王忠迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
